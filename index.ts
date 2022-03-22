e14613b9e2e3a85d904325ea41421578bb36a6a8
Jacobtread <jacobtread@gmail.com>
2022-03-22 13:20:57 +1300

    Removed test function

--- index.ts
+++ index.ts
@@ -410,26 +410,3 @@ export class PacketDefinition<T extends PacketStruct> {
         }
     }
 }
-
-
-const TestPacket = new PacketDefinition(0x02, {
-    name: DataType.String,
-    user: DataType.UInt8
-}, ['name', 'user'])
-
-export function Test() {
-    const socket = new BinarySocket('ws://localhost:8080/ws')
-    socket.definePacket(TestPacket)
-
-    socket.addListener(TestPacket, ({user, name}) => {
-        console.log(user, name)
-    })
-
-    socket.setEventListener('open', function () {
-        socket.send(TestPacket.create({
-            user: 2,
-            name: 'Test User'
-        }))
-    })
-}
-Test()
